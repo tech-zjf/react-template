81fd21d5ade919751c033a4c44db2fc2688e9545
tech-zjf <907572438@qq.com>
2025-10-28 14:13:55 +0800

    feat: 格式化

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,12 @@ import { useNavigate } from 'react-router';
 
 function App() {
     const [count, setCount] = useState(0);
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     return (
         <>
             <div className="flex justify-center gap-8 my-4 align-center">
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
-
                 </a>
                 <a href="https://react.dev" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo" />
@@ -21,7 +20,9 @@ function App() {
             </div>
             <h1 className="text-4xl font-bold text-center">Vite + React</h1>
             <div className="card">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount(count => count + 1)}>count is {count}</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount(count => count + 1)}>
+                    count is {count}
+                </button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
